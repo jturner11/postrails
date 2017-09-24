97bd04bfbdd9f1dcfa3896fc97c80e8a7e2ebc3c
jturner11 <joshturner11@hotmail.co.uk>
2017-09-24 21:23:03 +0100

    post and rails repo

---
+++ postRails.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POST AND RAILS</title>
+</head>
+<body>
+<?php
+    require("functions.php");
+?>
+<h1>Post and rails calculator</h1>
+<p> Specify the amount of post of railings to calculate the length of your fence</p>
+<form>
+    number of posts:<br>
+    <input type="text" name="posts"><br>
+    Last name:<br>
+    <input type="text" name="lastname">
+</form>
+</body>
+</html>
