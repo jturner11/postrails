2952702833e6df1ee72046257b96ff0507e60e4f
jturner11 <joshturner11@hotmail.co.uk>
2017-09-25 08:51:43 +0100

    part 2 fucntions

--- functions.php
+++ functions.php
@@ -1,15 +1,20 @@
 <?php
-
+if(isset($_POST["submit"])) {
+    $posts = $_POST['posts'] * 1;
+    $rails = $_POST['rails'] * 1;
+//    var_dump($posts, $rails);
+    echo Check($posts, $rails);
+}
 function Check($posts, $rails)
 {
     if ($posts > 1 && $rails > 0) {
-    $length = length($posts, $rails);
-    return "<span>Length:</span> " . $length . " m";
+        $length = length($posts, $rails);
+        return "<span>Length:</span> " . $length . " m";
     } else {
-    return "<span class='error'>" . "You haven't specified a valid amount !" . "</span>";
+        return "<span class='error'>" . "You haven't specified a valid amount !" . "</span>";
+    }
 }
-}
-    function length($posts, $rails)
+function length($posts, $rails)
 {
     $max_length = 0.1;
     while ($posts > 1 && $rails > 0) {
