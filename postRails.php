5dab407d56aa818a016755b8e68cffcf5102764f
jturner11 <joshturner11@hotmail.co.uk>
2017-09-25 11:42:07 +0100

    Just made code layout better and more readable

--- postRails.php
+++ postRails.php
@@ -4,17 +4,18 @@
     <title>POST AND RAILS</title>
 </head>
 <body>
-<h1>Post and rails calculator</h1>
-<p> Specify the amount of post of railings to calculate the length of your fence</p>
-<form action="postRails.php" method="post">
-    number of posts:<br>
-    <input type="text" name="posts"><br>
-    rails:<br>
-    <input type="text" name="rails">
-    <input type="submit" name="submit">
+
+    <h1>Post and rails calculator</h1>
+    <p> Specify the amount of post of railings to calculate the length of your fence</p>
+    <form action="postRails.php" method="post">
+            number of posts:<br>
+        <input type="text" name="posts"><br>
+            rails:<br>
+        <input type="text" name="rails">
+        <input type="submit" name="submit">
+    </form>
     <?php
         require("functions.php");
     ?>
-</form>
 </body>
 </html>
